75bd59d312066627822ac3c6126a42cd6284fcf1
Gio on Eris <github@gpiantoni.com>
2014-05-14 11:25:06 -0400

    it can replicate analysis, running epoch by epoch

--- spgr/detect_spindles.py
+++ spgr/detect_spindles.py
@@ -59,3 +59,44 @@ def calc_spindle_values(subj=None, detsp=None, ref_to_avg=None):
                      queue='short',
                      variables={'detsp': detsp})
     return [item for sublist in all_sp for item in sublist]
+
+
+def det_sp_in_one_epoch(one_trial_data=None):
+    from phypno.trans import Select, Math, Montage
+
+    calc_std = Math(operator_name='std', axis='time')
+    std_per_chan = calc_std(one_trial_data)
+    good_chan = where((std_per_chan(trial=0) > .001) & (std_per_chan(trial=0) < thresh))[0]
+    normal_chan = Select(chan=one_trial_data.axis['chan'][0][good_chan])
+
+    if ref_to_avg:
+        reref = Montage(ref_to_avg=True)
+        one_trial_data = reref(one_trial_data)
+
+    one_trial_data = normal_chan(one_trial_data)
+
+    spindles = detsp(one_trial_data)
+    return spindles.spindle
+
+
+def calc_spindle_values_one_epoch(subj=None, detection_options=None, ref_to_avg=None):
+
+    assert STAGE in STAGES.keys()
+
+    subj_dir = join(DATA_DIR, subj, REC_FOLDER)
+    data_file = glob(join(subj_dir, '*_' + STAGE + '.pkl'))[0]  # multiple files
+
+    with open(data_file, 'rb') as f:
+        data = load(f)
+
+    detsp = DetectSpindle(**detection_options)
+
+    all_sp = map_lsf(det_sp_in_one_epoch, iter(data),
+                     queue='short',
+                     variables={'detsp': detsp,
+                                'thresh': 300,
+                                'ref_to_avg': ref_to_avg},
+                     imports={'numpy': 'where'})
+
+    spindles = [x for sp in all_sp for x in sp]
+    return spindles
