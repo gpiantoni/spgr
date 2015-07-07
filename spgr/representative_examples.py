a0bd4e7d8ce79fc4fac45696495c1fbc519d5848
Gio Piantoni <github@gpiantoni.com>
2015-07-07 13:41:20 -0400

    representative examples works well but maybe there are better ways to find spindles

--- spgr/representative_examples.py
+++ spgr/representative_examples.py
@@ -21,14 +21,23 @@ YLIM = 150
 
 @with_log
 def Representative_Examples(lg, images_dir):
-
+    """Show some good spindle examples.
+    A better way of doing this is by doing fft at spindle, before and after and
+    take the spindle with the best ratio.
+    """
     lg.info('## Representative Spindles')
 
     lg.info('### Spindles in each brain region')
+
+    all_pos = []
+    all_title = []
+    all_png_file = []
+
     for subj in HEMI_SUBJ:
         best_spindles = find_best_spindles(subj)
 
         data = get_data(subj, 'sleep', CHAN_TYPE, reref='avg', **DATA_OPTIONS)
+        chan = data.attr['chan']
 
         for region, spindle in best_spindles.items():
 
@@ -39,16 +48,23 @@ def Representative_Examples(lg, images_dir):
                          trial=(i_trial, ), chan=(spindle['chan'], ))
             spindle_data = sel(data)
 
-            lg.info('### Representative for {} ({})'.format(region, subj))
-
             v = Viz1(color=PLOT_COLOR)
             v.add_data(spindle_data, limits_y=(-YLIM, YLIM))
             v.add_graphoelement([spindle, ])
 
             png_file = str(images_dir.joinpath('{}_{}.png'.format(region, subj)))
             v.save(png_file)
-            lg.info('![{}]({})'.format('{} {}'.format(subj, region),
-                    png_file))
+
+            y_pos = chan(lambda x: x.label == spindle['chan']).return_xyz()[0, 1]
+            all_pos.append(y_pos)
+            all_title.append('### Representative for {} ({})'.format(region,
+                                                                     subj))
+            all_png_file.append(png_file)
+
+    # sort by anterior-posterior axis
+    for _, title, png_file in sorted(zip(all_pos, all_title, all_png_file)):
+        lg.info(title)
+        lg.info('![]({})'.format(png_file))
 
 
 def find_best_spindles(subj):
