b11273b0b9a8d26636dfade96aab3a0584886d0b
Gio Piantoni <github@gpiantoni.com>
2015-07-09 17:37:44 -0400

    try representative example using sigma ratio

--- spgr/representative_examples.py
+++ spgr/representative_examples.py
@@ -1,15 +1,17 @@
+from numpy import where
 from phypno.trans import Select
 from phypno.viz import Viz1
+from scipy.signal import periodogram
 
-from spgr.constants import (CHAN_TYPE,
-                            DATA_OPTIONS,
-                            HEMI_SUBJ,
-                            PLOT_COLOR,
-                            SPINDLE_OPTIONS,
-                            )
-from spgr.detect_spindles import get_spindles
-from spgr.read_data import get_data
-from spgr.spindle_source import get_chan_with_regions
+from .constants import (CHAN_TYPE,
+                        DATA_OPTIONS,
+                        HEMI_SUBJ,
+                        PLOT_COLOR,
+                        SPINDLE_OPTIONS,
+                        )
+from .detect_spindles import get_spindles
+from .read_data import get_data
+from .spindle_source import get_chan_with_regions
 
 from .log import with_log
 
@@ -17,6 +19,9 @@ from .log import with_log
 REREF = 'avg'
 PAD = 2
 YLIM = 150
+WINDOW = 1
+
+BEST_SPINDLE = 'sigma_ratio'  # 'area_under_curve' or 'sigma_ratio'
 
 
 @with_log
@@ -34,20 +39,14 @@ def Representative_Examples(lg, images_dir):
     all_png_file = []
 
     for subj in HEMI_SUBJ:
-        best_spindles = find_best_spindles(subj)
 
         data = get_data(subj, 'sleep', CHAN_TYPE, reref='avg', **DATA_OPTIONS)
+        best_spindles = find_best_spindles(subj, data)
         chan = data.attr['chan']
 
         for region, spindle in best_spindles.items():
 
-            i_trial = find_trial_with_spindle(data, spindle)
-
-            sel = Select(time=(spindle['start_time'] - PAD,
-                               spindle['end_time'] + PAD),
-                         trial=(i_trial, ), chan=(spindle['chan'], ))
-            spindle_data = sel(data)
-
+            spindle_data = find_spindle_data(data, spindle)
             v = Viz1(color=PLOT_COLOR)
             v.add_data(spindle_data, limits_y=(-YLIM, YLIM))
             v.add_graphoelement([spindle, ])
@@ -67,7 +66,7 @@ def Representative_Examples(lg, images_dir):
         lg.info('![]({})'.format(png_file))
 
 
-def find_best_spindles(subj):
+def find_best_spindles(subj, data):
 
     spindles = get_spindles(subj, reref=REREF, **SPINDLE_OPTIONS)
     chan = get_chan_with_regions(subj, REREF)
@@ -82,8 +81,15 @@ def find_best_spindles(subj):
 
         sp_region = sp_region[len('ctx-Xh-'):]
 
+        # there are different ways to define the best spindle.
+        # the quality of a spindle will be stored in goodness (the higher the better)
+        if BEST_SPINDLE == 'area_under_curve':
+            one_sp['goodness'] = one_sp['area_under_curve']
+        elif BEST_SPINDLE == 'sigma_ratio':
+            one_sp['goodness'] = _find_sigma_ratio(one_sp, data)
+
         if sp_region in best_spindles:
-            if one_sp['area_under_curve'] > best_spindles[sp_region]['area_under_curve']:
+            if one_sp['goodness'] > best_spindles[sp_region]['goodness']:
                 best_spindles[sp_region] = one_sp
         else:
             best_spindles[sp_region] = one_sp
@@ -91,7 +97,43 @@ def find_best_spindles(subj):
     return best_spindles
 
 
-def find_trial_with_spindle(data, sp):
+def find_spindle_data(data, spindle):
+    i_trial = _find_trial_with_spindle(data, spindle)
+
+    sel = Select(time=(spindle['start_time'] - PAD,
+                       spindle['end_time'] + PAD),
+                 trial=(i_trial, ), chan=(spindle['chan'], ))
+    return sel(data)
+
+
+def _find_trial_with_spindle(data, sp):
     for i, t in enumerate(data.axis['time']):
         if sp['start_time'] > t[0] and sp['start_time'] < t[-1]:
             return i
+
+
+def _find_sigma_ratio(one_sp, data):
+
+    tb_0 = one_sp['peak_time'] - WINDOW / 2
+    ib_0 = where(data.axis['time'][0] >= tb_0)[0][0]
+    ib_1 = ib_0 + WINDOW * data.s_freq
+
+    ia_0 = ib_0 - 1 * WINDOW * data.s_freq
+    ia_1 = ib_0 - 0 * WINDOW * data.s_freq
+    ic_0 = ib_0 + 1 * WINDOW * data.s_freq
+    ic_1 = ib_0 + 2 * WINDOW * data.s_freq
+
+    ia = data.data[0][0, ia_0:ia_1]
+    ib = data.data[0][0, ib_0:ib_1]
+    ic = data.data[0][0, ic_0:ic_1]
+
+    sigma_ratio = sigma_power(ib, data.s_freq) / (sigma_power(ia, data.s_freq) +
+                                                  sigma_power(ic, data.s_freq))
+
+    return sigma_ratio
+
+
+def sigma_power(x, s_freq):
+    [f, Pxx] = periodogram(x, fs=s_freq, nfft=s_freq)
+    SIGMA_FREQ = SPINDLE_OPTIONS['frequency']
+    return sum(Pxx[(f >= SIGMA_FREQ[0]) & (f <= SIGMA_FREQ[1])])
