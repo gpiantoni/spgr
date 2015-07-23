d498652c3acf7b522650c8194838e77fa1f02747
Gio Piantoni <github@gpiantoni.com>
2015-07-23 14:18:32 -0400

    REMOVED spindle detection, but show multiple channels

--- spgr/spindle_detection_method.py
+++ spgr/spindle_detection_method.py
@@ -24,7 +24,7 @@ from .log import with_log
 PLOT_SIZE = 960, 240
 
 SUBJ = 'EM09'
-chan_name = 'GR50'
+chan_names = ['GR28', 'GR50']
 start_good_time = 44850
 end_good_time = 44880
 
@@ -43,78 +43,28 @@ def Spindle_Detection_Method(lg, images_dir):
                       dur0=SPINDLE_OPTIONS['duration'][0],
                       dur1=SPINDLE_OPTIONS['duration'][1]))
 
-    lg.info('Example from {subj} on chan {chan} at {time} s'
-            ''.format(subj=SUBJ, chan=chan_name, time=start_good_time))
+    lg.info('Example from {subj} at {time} s'
+            ''.format(subj=SUBJ, time=start_good_time))
 
     data = get_data(SUBJ, 'sleep', CHAN_TYPE, **DATA_OPTIONS)
 
     good_trial = [i for i, trl in enumerate(data)
                   if start_good_time // 30 * 30 == trl.axis['time'][0][0]][0]
 
-    sel = Select(trial=(good_trial, ), chan=(chan_name, ))
+    sel = Select(trial=(good_trial, ), chan=chan_names)
     sel_data = sel(data)
-
-    v = Viz1(color=PLOT_COLOR)
-    v.size = PLOT_SIZE
-    v.add_data(sel_data, limits_x=(start_good_time, end_good_time),
-               limits_y=RAW_LIMITS_Y)
-    v._plots[chan_name].setLabels(left='amplitude (μV)', bottom='time (s)')
-
-    png_file = str(images_dir.joinpath('raw.png'))
-    v.save(png_file)
-    lg.info('![{}]({})'.format('Raw EEG', png_file))
 
     detsp = DetectSpindle(method=SPINDLE_OPTIONS['method'],
                           frequency=SPINDLE_OPTIONS['frequency'],
                           duration=SPINDLE_OPTIONS['duration'])
     sp = detsp(sel_data)
 
-    filt = Filter(low_cut=detsp.det_butter['freq'][0],
-                  high_cut=detsp.det_butter['freq'][1],
-                  order=detsp.det_butter['order'], s_freq=data.s_freq)
-    x_filt = filt(sel_data)
-
-    def _move_avg(dat, axis):
-        """Function to compute moving average."""
-        flat = ones(detsp.moving_avg['dur'] * data.s_freq)
-
-        dat = swapaxes(dat, axis, -1)
-        for i, x in enumerate(dat):
-            dat[i, :] = fftconvolve(x, flat / sum(flat), mode='same')
-        return swapaxes(dat, -1, axis)
-
-    hilbert_abs = Math(operator_name=('hilbert', 'abs'), axis='time')
-    moving_avg = Math(operator=_move_avg, axis='time')
-
-    dat_det = moving_avg(hilbert_abs(x_filt))
-
-    v = Viz1(color=PLOT_COLOR)
-    v.size = PLOT_SIZE
-    v.add_data(x_filt, color=mkPen('b', width=1),
-               limits_x=(start_good_time, end_good_time), limits_y=(-100, 100))
-    v.add_data(dat_det, color=mkPen('r', width=4))
-    v._plots[chan_name].setLabels(left='amplitude (μV)', bottom='time (s)')
-
-    png_file = str(images_dir.joinpath('envelope.png'))
-    v.save(png_file)
-    lg.info('![{}]({})'.format('Envelope', png_file))
-
-    t = dat_det.axis['time'][0]
-    good_sp = [x for x in sp.spindle
-               if x['start_time'] < t[-1] and x['end_time'] > t[0]]
-
-    detected_spindles = deepcopy(dat_det)
-
-    for one_sp in good_sp:
-        i0 = where(t == one_sp['start_time'])[0]
-        i1 = where(t == one_sp['end_time'])[0]
-        detected_spindles.data[0][0, i0:i1] = NaN
-
     v = Viz1(color=PLOT_COLOR)
     v.size = PLOT_SIZE
     v.add_data(sel_data, limits_x=(start_good_time, end_good_time),
                limits_y=RAW_LIMITS_Y)
-    v._plots[chan_name].setLabels(left='amplitude (μV)', bottom='time (s)')
+    v._plots[chan_names[-1]].setLabels(left='amplitude (μV)',
+                                       bottom='time (s)')
     v.add_graphoelement(sp)
     png_file = str(images_dir.joinpath('detected.png'))
     v.save(png_file)
